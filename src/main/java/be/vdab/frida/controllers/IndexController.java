ee355022c78bd181caadc421cbb2ecb65abe28b4
andre <andre.komdeur@gmail.com>
2020-03-31 13:57:32 +0200

    1.16

--- src/main/java/be/vdab/frida/controllers/IndexController.java
+++ src/main/java/be/vdab/frida/controllers/IndexController.java
@@ -3,19 +3,21 @@ package be.vdab.frida.controllers;
 import be.vdab.frida.domain.Adres;
 import be.vdab.frida.domain.Gemeente;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-// enkele imports
 @Controller
 @RequestMapping("/")
 class IndexController {
     @GetMapping
-    public ModelAndView index() {
+    public ModelAndView index(@CookieValue(name="kleur", required = false) String kleur,@CookieValue(name = "reedsBezocht", required = false)
+            String reedsBezocht, HttpServletResponse response) {
         DayOfWeek weekdag = LocalDate.now().getDayOfWeek();
         String openGesloten =
                 weekdag == DayOfWeek.MONDAY || weekdag == DayOfWeek.THURSDAY ?
@@ -24,5 +26,13 @@ class IndexController {
                 new ModelAndView("index", "openGesloten", openGesloten);
         modelAndView.addObject(
                 new Adres("Grote markt", "7", new Gemeente("Brussel", 1000)));
+        modelAndView.addObject("kleur",kleur);
+        Cookie cookie = new Cookie("reedsBezocht", "ja");
+        cookie.setMaxAge(31_536_000);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        if (reedsBezocht != null) {
+            modelAndView.addObject("reedsBezocht", true);
+        }
         return modelAndView;    }
 }
--- src/main/java/be/vdab/frida/controllers/TaalController.java
+++ src/main/java/be/vdab/frida/controllers/TaalController.java
@@ -7,23 +7,17 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
-
 @Controller
-@RequestMapping("os")
-class OSController {
-    private static final String[] OSS = {"Windows", "Macintosh", "Android", "Linux"};
-
+@RequestMapping("taal")
+class TaalController {
     @GetMapping
-    public ModelAndView os(
-            @RequestHeader("User-Agent") String userAgent,
+    public ModelAndView nederlands(
+            @RequestHeader("Accept-Language") String language,
             @CookieValue(name = "kleur", required = false) String kleur) {
-        ModelAndView modelAndView = new ModelAndView("os");
-        Arrays.stream(OSS)
-                .filter(os -> userAgent.contains(os))
-                .findFirst()
-                .ifPresent(
-                        os -> modelAndView.addObject("os", os));
+        ModelAndView modelAndView = new ModelAndView(
+                "taal",
+                "nederlands",
+                language.startsWith("nl"));
         modelAndView.addObject("kleur", kleur);
         return modelAndView;
     }
